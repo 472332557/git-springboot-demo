211fac402eb01f160a01d79c3d249cde50b8f9be
liangzc <472332557@qq.com>
2021-12-01 18:32:26 +0800

    jdk httpUrlConnection练习

--- src/main/java/com/liangzc/example/start/servlet/ServletTest.java
+++ src/main/java/com/liangzc/example/start/servlet/ServletTest.java
@@ -11,7 +11,7 @@ import java.io.IOException;
  * 引入servlet
  */
 
-@WebServlet(urlPatterns = "/start/*")
+//@WebServlet(urlPatterns = "/start/*")
 public class ServletTest extends HttpServlet {
 
     @Override
